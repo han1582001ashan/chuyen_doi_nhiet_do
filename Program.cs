a9e3c14eee251ef619f6b315b314ac7727b5e4d9
han1582001ashan <83277320+han1582001ashan@users.noreply.github.com>
2024-08-26 20:25:14 +0700

    nopbai

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace ChuyenDoiNhietDo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahrenheit;
+            double celsius;
+            int choice;
+            do
+            {
+
+
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("1. Fahrenheit to Celsius.");
+                Console.WriteLine("2. Celsius to Fahrenheit.");
+                Console.WriteLine("0. Exit");
+                choice = Int32.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter your Fahrenheit: ");
+                        fahrenheit = Double.Parse(Console.ReadLine());
+                        Console.WriteLine("-->" + FahrenheitToCelsius(fahrenheit));
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter your Celsius");
+                        celsius = Double.Parse(Console.ReadLine());
+                        Console.WriteLine("-->" + CelsiusToFahrenheit(celsius));
+                        break;
+                }
+
+            } while (choice != 0);
+
+
+
+        }
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            double fahrenheit = (9.0 / 5) * celsius + 32;
+            return fahrenheit;
+        }
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            double celsius = (5.0 / 9) * (fahrenheit - 32);
+            return celsius;
+        }
+
+
+    }
+}
+
